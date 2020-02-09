d6a8ad5fee3341c1c3a31ea2871ae3b2ab9fc9ce
GUANG GUANG <31147144+lungchu@users.noreply.github.com>
2020-02-09 10:46:33 +0800

    rule base

---
+++ ml_play.py
@@ -0,0 +1,75 @@
+"""The template of the main script of the machine learning process
+"""
+
+import games.arkanoid.communication as comm
+from games.arkanoid.communication import ( \
+    SceneInfo, GameStatus, PlatformAction
+)
+
+def getTargetX(ball,ball_Last):
+    move_X=ball[0]-ball_Last[0]
+    move_Y=ball[1]-ball_Last[1]
+    # 3.4. Send the instruction for this frame to the game process
+    if move_Y<0 :
+        return 90
+    else:
+        #2.碰到左右時X方向->反向
+        #3.算出X的目標值
+        ball2platform_time=(395-ball[1])/move_Y
+        targetX=ball[0]+move_X*ball2platform_time
+        #    print('move_x=',move_X,'down_Y=',down_Y,' targetX=',targetX)
+
+        while (targetX<0) or (targetX>200):
+            if targetX<0:
+                targetX=-targetX
+            else:
+                targetX=400-targetX
+
+    return(targetX)
+    print('move_x=',move_X,'move_Y=',move_Y,'ball X=',ball[0],' ball Y=',ball[1],' target X=',targetX)
+
+
+
+
+def ml_loop():
+    """The main loop of the machine learning process
+
+    This loop is run in a separate process, and communicates with the game process.
+
+    Note that the game process won't wait for the ml process to generate the
+    GameInstruction. It is possible that the frame of the GameInstruction
+    is behind of the current frame in the game process. Try to decrease the fps
+    to avoid this situation.
+    """
+
+    # === Here is the execution order of the loop === #
+    # 1. Put the initialization code here.
+    ball_last = [101,101]
+    # 2. Inform the game process that ml process is ready before start the loop.
+    comm.ml_ready()
+
+    # 3. Start an endless loop.
+    while True:
+        # 3.1. Receive the scene information sent from the game process.
+        scene_info = comm.get_scene_info()
+
+        # 3.2. If the game is over or passed, the game process will reset
+        #      the scene and wait for ml process doing resetting job.
+        if scene_info.status == GameStatus.GAME_OVER or \
+            scene_info.status == GameStatus.GAME_PASS:
+            # Do some stuff if needed
+
+            # 3.2.1. Inform the game process that ml process is ready
+            comm.ml_ready()
+
+        # 3.3. Put the code here to handle the scene information
+        #1.算出下降速率
+        targetX=getTargetX(scene_info.ball,ball_last)
+        ball_last=scene_info.ball
+        platform_X=scene_info.platform[0]
+        if (platform_X+18) > (targetX) :
+            comm.send_instruction(scene_info.frame, PlatformAction.MOVE_LEFT)
+        elif (platform_X+18) < (targetX) :
+            comm.send_instruction(scene_info.frame, PlatformAction.MOVE_RIGHT)
+        else:
+            comm.send_instruction(scene_info.frame, PlatformAction.NONE)
